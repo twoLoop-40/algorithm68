9332c936ab8332cd7e80643c1c3ef505a9eecfd0
Joon Ho Lee <jh.lee@spira-t.com>
2022-03-23 14:19:51 +0900

    q01

--- chapter1/q01.js
+++ chapter1/q01.js
@@ -1,1 +1,35 @@
-function findSymmetricNumber(from = 10) {}
+function findSymmetricNumber(from = 10) {
+  const transformNumberByDigit = (num) => [
+    num.toString(2),
+    num.toString(8),
+    num.toString(),
+  ];
+  const checkSymmetry = (item = "") => {
+    if (item.length == 0 || item.length == 1) return true;
+    return (
+      item[0] === item[item.length - 1] &&
+      checkSymmetry(item.slice(1, item.length - 1))
+    );
+  };
+  const symmetricInString = (numString) => {
+    if (!Array.isArray(numString)) {
+      return checkSymmetry(numString) ? true : false;
+    } else {
+      return numString.reduce(
+        (prevBool, item) => prevBool && checkSymmetry(item),
+        true
+      );
+    }
+  };
+  if (symmetricInString(transformNumberByDigit(from))) {
+    return from;
+  } else {
+    return findSymmetricNumber(from + 1);
+  }
+}
+
+function runQ01() {
+  console.log(findSymmetricNumber(10));
+}
+
+runQ01();
