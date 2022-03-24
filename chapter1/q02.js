293b3274d0aed189e2d16606340dfed51a005556
Joon Ho Lee <jh.lee@spira-t.com>
2022-03-24 16:30:26 +0900

    try2

--- chapter1/q02.js
+++ chapter1/q02.js
@@ -1,13 +1,26 @@
 function makeExpForCalc(num) {
-  const addOp = (num) => {
-    if (num.length <= 1) return num;
-  };
   const changeToString = (num) =>
     typeof num === "string" ? num : num.toString();
-  if (!Array.isArray(num)) {
-    num = changeToString(num);
-    return addOp(num);
-  } else {
-    return num.map((numStr) => makeExpForCalc(numStr)).flat();
+  if (!num) {
+    console.log("RETURN FOR NOTHING");
+    return;
   }
+  const addOperations = (exp) => {
+    if (typeof exp === "string") {
+      return [exp, "+" + exp, "*" + exp, "-" + exp];
+    } else if (Array.isArray(exp)) {
+      return exp.flatMap((exp) => addOperations(exp));
+    }
+  };
+  const mathExp = (nums) => {
+    if (nums.length <= 1) return nums;
+    const head = nums[0];
+    return addOperations(mathExp(nums.slice(1))).map((exp) => head + exp);
+  };
+  return mathExp(changeToString(num));
 }
+
+function runQ02() {
+  console.log(makeExpForCalc(1321));
+}
+runQ02();
