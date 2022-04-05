14eab00b813d1fa2b1a6984688b3712f6a494c29
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-05 14:20:57 +0900

    q09 finish

--- chapter1/src/index.ts
+++ chapter1/src/index.ts
@@ -15,22 +15,25 @@
 	 }
 	const twoDimBin = makeTwoDimStorage(m, w)
 	const findPath = (men: number, women: number): number => {
-		if (men == women) return 0
-		if (men > m || women > w) return 0
 		if (men == m && women == w) return 1
+		if (men > m || women > w) return 0
+		if (men == women) return 0
+		if (m - men == w - women) return 0
 		if (twoDimBin(men, women) != 0) return twoDimBin(men, women)
 		else {
 			const value = findPath(men + 1, women) + findPath(men, women + 1)
 			return twoDimBin(men, women, value)
 		}
 	 }
-	 if (m == w) return 0
-	 else if (m > w) return findPath(1, 0)
+	 twoDimBin(0, 0, 1)
+	 if (m == w) return findPath(1, 0) + findPath(0, 1)
+	 if (m > w) return findPath(1, 0)
 	 else return findPath(0, 1)
+	 
 }
 
 function runQ09 () {
-	console.log(findNotSameMenWomen(19, 10))
+	console.log(findNotSameMenWomen(20, 10))
 }
 
 runQ09()
