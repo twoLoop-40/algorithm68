28cb65e80c2cf1f5b344f18831bf3144ab55e1e7
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-08 17:55:24 +0900

    q11 second

--- chapter2/src/index.ts
+++ chapter2/src/index.ts
@@ -1,5 +1,4 @@
-
-function fibFilter (argNum: number): number {
+function fibFilter (limit: number) {
 	const digitSum = (num: number, result: number = 0): number => {
 		if (num == 0) return result
 		else return digitSum(
@@ -10,35 +9,31 @@ function fibFilter (argNum: number): number {
 	const digitSumDivide = (divisor: number, dividend: number): boolean => {
 		return dividend % digitSum(divisor) === 0
 	}
-	const fibIter = (function *() {
+	const fibGen = function *() {
 		let prev = 1
 		let curr = 1
 		let result: number[] = []
-		let limit = yield
-		while(result.length < limit) {
+		while (result.length < limit) {
 			[prev, curr] = [curr, prev + curr]
-			if(digitSumDivide(prev, prev)) {
-				result.push(prev)
-			}
-			yield result
+			let effect: boolean = yield curr
+			if (effect) result.push(curr)	
 		}
 		return result
-	})()
-	const filter = <T>(iter: IterableIterator<T>) => {
-		const { value, done } = iter.next()
+	}
+	const filter = (decision: boolean = false, iter: Generator<number, number[], boolean>): number[] => {
+		const { value, done } = iter.next(decision)
 		if (done) return value
 		else {
-			console.log('current result:', value)
-			return filter(iter)
+			decision = digitSumDivide(value, value)
+			return filter(decision, iter)
 		}
 	}
-	fibIter.next()
-	fibIter.next(argNum)
-	return filter(fibIter)
+	return filter(false, fibGen())
+	
 }
 
 function runQ11 () {
-	console.log(fibFilter(10))
+	console.log(fibFilter(5))
 }
 
 runQ11()
