9f8055da01f3a94704e4ce39bff338e5bff44a01
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-10 01:46:24 +0900

    modify

--- chapter2/src/index.ts
+++ chapter2/src/index.ts
@@ -1,4 +1,13 @@
 function findAllDigits (irrational: number): number {
+	const makeStartNumber = (irrational: number): number => {
+		if (irrational < 10 && irrational > 1) return (irrational / 10)
+		if (irrational > 10) return makeStartNumber(irrational / 10)
+		if (irrational < 1 && irrational > 0) return makeStartNumber(irrational * 10)
+		else {
+			console.log('WROND INPUT. NUMBER MUST BE POSITIVE IRRATIONAL')
+			return -1
+		}
+	}
 	const getFirstDigit = (betweenOneZero: number) => {
 		const head = Math.floor(betweenOneZero * 10)
 		const tail = betweenOneZero * 10 - head
@@ -12,8 +21,13 @@ function findAllDigits (irrational: number): number {
 			digits = yield head
 		}
 	}
-	const allDigits = (scaler: Generator<number, number, number>): number => {
-		
+	const allDigits = (scaler: Generator<number, number, number>, 
+		digitArray: number[] = Array.from({ length: 10 }, () => -1), count: number = 0): number => {
+		if (digitArray.filter(digit => digit > -1).length == 10) return count
+		else {
+
+			
+		}
 	}
 }
 function runQ11 () {
